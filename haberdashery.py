381d75a01ead9b1b1a9c033f26f948aa4004f9a5
Jrabbit <jackjrabbit+github@gmail.com>
2010-06-01 23:12:06 -0500

    stupid mistakes.

--- haberdashery.py
+++ haberdashery.py
@@ -19,38 +19,59 @@ for x in managers:
 	else:
 		paths.append(None) 
 		#this may seem redundant but if brew isn't installed and then port is, calling the 2nd key would give the macports path.
+	if len(paths) == 0:
+		print "Sorry found no package managers. Is your $PATH setup correctly?"
 
 def install(pac):
 	"""Try and install a package"""
+	pass
 
 def search_fink(pac):
-	""" Searches fink for a package"""
-	things = Popen(["fink", "list", pac], stdout=PIPE).communicate()
-	if things[0] = 0:
+	""" Searches fink for a package, returns a list of packages line by line"""
+	things = Popen(["fink", "list", pac], stdout=PIPE).communicate()[0]
+	if things == 0:
 		return
-	else
-		theone = things[0]
-		print("found " + len(theone.split("\n") + " results for " + pac + " in your Fink")
-		print theone
-		return theone
+	else:
+		print("found " + len(things.split("\n")) + " results for " + pac + " in your Fink:")
+		print things
+		return things
 # TODO: add a way to list the installed/uninstalled status using indices. Growl support?
+
+def search_brew(pac):
+	"""search homebrew for a package"""
+	things = Popen(["brew", "search", pac], stdout=PIPE).communicate()[0]
+	things = things.split("\n").pop()
+	if len(things) == 0:
+		return
+	else:
+		print("found " + len(things) + " results for " + pac + " in your homebrew:")
+		print things
+		return things
+
+def search_port(pac):
+	"""Search macports for a package, returns a list of package lines"""
+	things = Popen(["port", "search", pac], stdout=PIPE).communicate()[0]
+	if things == 0:
+		return
+	else:
+		print("found " + len(things.split("\n")) + " results for " + pac + " in your MacPorts:")
+		print things
+		return things
 
 def search(pac):
 	"""Find a package in fink/brew/ports then run whohas"""
 
-
 def edit(pac, man):
 	"""open a package description in $EDITOR"""
+	#TODO: take editor from commandline
 	#fink has no edit function
-	if man = fink:
+	if man == fink:
 		#fink dumpinfo -finfofile pac | cut -d: -f2 | xargs $editor
 		rawdump = Popen(["fink", "dumpinfo", "-finfofile", pac], stdout=PIPE).communicate()[0]
 		os.system("open " + rawdump.split(":")[1])
-	
-	elif man = brew:
+		#this might need adjustments based on if .info files are asociated
+	elif man == brew:
 		os.system("brew edit" + pac)
-	
-	elif man = port:
+	elif man == port:
 		os.system("port edit" + pac)
-	
 
