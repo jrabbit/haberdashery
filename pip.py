ff966870dab801d2767929369bfa6297127d9372
Jrabbit <jackjrabbit+github@gmail.com>
2012-01-30 00:06:04 -0500

    problem with pip checking

--- pip.py
+++ pip.py
@@ -1,7 +1,6 @@
 from subprocess import *
-import sys
 #pip = sys.argv[0]
 def is_python():
     pip = Popen(['pip', '--version'], stdout=PIPE).communicate()[0]
-    if pip[-12:].split() == "(python":
+    if pip.strip()[-12:].split() == "(python":
         return True
