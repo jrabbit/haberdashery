8d9f045f9ab8cc6fb447bb1f5a95a66f56bf7a2f
Jrabbit <jackjrabbit+github@gmail.com>
2012-01-30 00:24:45 -0500

    whoops.

--- pip.py
+++ pip.py
@@ -2,5 +2,5 @@ from subprocess import *
 #pip = sys.argv[0]
 def is_python():
     pip = Popen(['pip', '--version'], stdout=PIPE).communicate()[0]
-    if pip.strip()[-12:].split() == "(python":
+    if pip.strip()[-12:].split()[0] == "(python":
         return True
